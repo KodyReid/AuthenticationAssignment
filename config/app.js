54b8db28fa9851ee3b485ae410bf40613a6c9001
Kody <kody.reid7@gmail.com>
2021-06-15 09:58:52 -0400

    added db.js and hid app.js

--- config/app.js
+++ config/app.js
@@ -11,6 +11,19 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+//database setup
+let mongoose = require('mongoose');
+let DB = require('./db');
+
+// point mongoose to the DB URI
+mongoose.connect(DB.URI, {useNewUrlParser: true, useUnifiedTopology: true });
+
+let mongoDB = mongoose.connection;
+mongoDB.on('error', console.error.bind(console,'Connection Error: '));
+mongoDB.once('open', ()=>{
+  console.log('Connected to MongoDB...');
+});
+
 let indexRouter = require('../routes/index');
 let usersRouter = require('../routes/users');
 
