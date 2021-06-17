04ac724ca529c196268dea02bf94a4124b764ba3
Kody <kody.reid7@gmail.com>
2021-06-17 13:25:44 -0400

    moved to mongoatlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://localhost/contacts"
+    //"URI": "mongodb://localhost/contacts"
+    "URI": "mongodb+srv://kody:nuMGO7pOxjRZThnO@dbserver.z9qnb.mongodb.net/contacts?retryWrites=true&w=majority"
 }
